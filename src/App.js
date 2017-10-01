3483acc9b055b8c3108a4f39037be75109dc5e9e
Élysson MR <elysonmr@gmail.com>
2017-10-01 20:01:44 -0300

    Add bootstrap and a state example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Ola from './ola'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => this.setState({count: this.state.count + 1}), 1000)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Ola name="Elysson"></Ola>
+        <p>Count: {this.state.count}</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/ola.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Ola extends Component {
+    render() {
+        return <p>Olá {this.props.name}</p>
+    }
+}
+
+
+export default Ola
